a5f41570b2299e5265d0195cb7901f9081713bac
Pawel Baran <pawelbaranc@gmail.com>
2022-04-11 21:05:50 +0200

    dodano zadanie domowe 11.04.2022

---
+++ Zadanie_C_sharp_20220411/Zadanie_C_sharp_20220411/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+
+namespace Zadanie_C_sharp_20220411
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LetterToAsci();
+        }
+
+        private static void LetterToAsci()
+        {
+            char providedKeyChar;
+            bool doWhile = true;
+            do
+            {
+                Console.WriteLine("Please press any key, program will check if the key is digid or functional kay, \notherwise will return ASCI number:\n");
+                var providedKey = Console.ReadKey();
+                Console.WriteLine();
+                providedKeyChar = providedKey.KeyChar;
+                if (char.IsDigit(providedKeyChar) == true)
+                {
+                    Console.WriteLine("Key is digid");
+                }
+                else if (char.IsControl(providedKeyChar) == true)
+                {
+                    Console.WriteLine("Is a functional kay");
+                }
+                else if (providedKeyChar == 'q' || providedKeyChar == 'Q')
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine((int)providedKeyChar);
+                }
+                doWhile = Menu(doWhile);
+            } while (doWhile);
+        }
+
+        private static bool Menu(bool doWhile)
+        {
+            Console.WriteLine("\nPress any key to continue or [q] to exit:");
+            var menuKey = Console.ReadKey();
+            char menuKeyChar = menuKey.KeyChar;
+            if (menuKeyChar == 'q' || menuKeyChar == 'Q')
+            {
+                doWhile = false;
+            }
+            else
+            {
+                Console.Clear();
+            }
+
+            return doWhile;
+        }
+    }
+}
